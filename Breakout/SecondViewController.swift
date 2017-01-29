70171199ecb1219e7538201024082d8ff2b6d341
dKirill <1starfall1@gmail.com>
2017-01-29 15:53:22 +0300

    Initial Commit

---
+++ Breakout/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Breakout
+//
+//  Created by Кирилл Делимбетов on 29.01.17.
+//  Copyright © 2017 Кирилл Делимбетов. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
